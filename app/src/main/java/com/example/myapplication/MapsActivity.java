8d6bd1b08ee19ca15293ce162bb1e67840b03e1c
hp <johnferijr@gmail.com>
2023-03-29 14:54:54 +0700

    commit

--- app/src/main/java/com/example/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -56,6 +56,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        setMapOnClick(mMap);
+        setPoiClicked(mMap);
+        enableMyLocation();
     }
 
     private void setMapOnClick(final GoogleMap map) {
